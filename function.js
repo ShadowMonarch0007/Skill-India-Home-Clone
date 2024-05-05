8a8b05316a1bfad7e0103b12b651513cdc8b76ac
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-06 03:35:08 +0530

    05.05.2024

--- function.js
+++ function.js
@@ -1,13 +1,43 @@
-function myFunction() {
-    document.getElementsByTagName("img").style.filter = "grayscale(100%)";
-  }
-//   var b= document.getElementById("id1");
-//   function show(a){
-//     document.getElementById(a).style.display="block";
-//   }
-  function show(){
-    document.getElementById("showsearchbox").style.display="block";
-  }
-  function showmore(){
-    document.getElementById("morebox").style.display="block";
-  }
+function show(idName) {
+    var element = document.getElementById(idName);
+    element.style.display = "block";
+}
+function hide(idName) {
+    var element = document.getElementById(idName);
+    element.style.display = "none";
+}
+var overlay = document.createElement("div");
+function showregibtn(idName) {
+
+    var element = document.getElementById(idName);
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100vw";
+    overlay.style.height = "100vh";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    overlay.style.zIndex = "999";
+    document.body.appendChild(overlay);
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    var elementWidth = element.offsetWidth;
+    var elementHeight = element.offsetHeight;
+    var centerX = (screenWidth - elementWidth) / 4;
+    var centerY = (screenHeight - elementHeight) / 2;
+    element.style.position = "fixed";
+    element.style.top = centerY + "px";
+    element.style.left = centerX + "px";
+    element.style.display = "block";
+    element.style.zIndex = "1000";
+    overlay.addEventListener("mousedown", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+}
+function hideregibtn(idName) {
+    var element = document.getElementById(idName);
+    overlay.style.zIndex = "-999";
+    overlay.style.backgroundColor = "transparent";
+    element.style.display = "none";
+
+}
