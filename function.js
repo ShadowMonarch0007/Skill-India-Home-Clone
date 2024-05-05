34b1461530fe66bd88dea48edbd91dd6f2f50e26
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-05 13:54:28 +0530

    05.05.2024

---
+++ function.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    document.getElementsByTagName("img").style.filter = "grayscale(100%)";
+  }
+  var b= document.getElementById("id1");
+  function show(a){
+    document.getElementById(a).style.display="block";
+  }
+//   function show(){
+//     document.getElementById("showsearchbox").style.display="block";
+//   }
+//   function showmore(){
+//     document.getElementById("morebox").style.display="block";
+//   }
