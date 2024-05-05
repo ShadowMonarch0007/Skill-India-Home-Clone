3b173fcf67f46a6cd2708342dbc4f184a6d46032
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-06 04:12:37 +0530

    05.05.2024

--- function.js
+++ function.js
@@ -34,6 +34,33 @@ function showregibtn(idName) {
         event.stopPropagation();
     });
 }
+function showregibtnmob(idName) {
+
+    var element = document.getElementById(idName);
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100vw";
+    overlay.style.height = "100vh";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    overlay.style.zIndex = "999";
+    document.body.appendChild(overlay);
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    var elementWidth = element.offsetWidth;
+    var elementHeight = element.offsetHeight;
+    var centerX = (screenWidth - elementWidth) / 10;
+    var centerY = (screenHeight - elementHeight) / 2;
+    element.style.position = "fixed";
+    element.style.top = centerY + "px";
+    element.style.left = centerX + "px";
+    element.style.display = "block";
+    element.style.zIndex = "1000";
+    overlay.addEventListener("mousedown", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    });
+}
 function hideregibtn(idName) {
     var element = document.getElementById(idName);
     overlay.style.zIndex = "-999";
